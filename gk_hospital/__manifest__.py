4c4fb6fa4d2af92352c72c9ac72dadc080db8bdb
grigoriykosarev@gmail.com <grigoriykosarev@gmail.com>
2023-05-31 07:40:21 -0700

    init

--- gk_hospital/__manifest__.py
+++ gk_hospital/__manifest__.py
@@ -3,33 +3,31 @@
     'name': "gk_hospital",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Odoo-school. Module 'Hospital'
+        """,
 
     'description': """
-        Long description of module's purpose
-    """,
+        Odoo-school. Module 'Hospital'
+        """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author':  "grigoriykosarev@gmail.com",
+    'website': "grigoriykosarev@gmail.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Customizations',
+    'version': '15.0.1.0.0',
+    'license': 'OPL-1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
+
+    'installable': True,
+    'auto_install': False,
+
     'demo': [
-        'demo/demo.xml',
     ],
 }
---
+++ gk_library/models/doctor.py
@@ -0,0 +1,14 @@
+import logging
+
+from odoo import models, fields, api
+
+
+class Doctor(models.Model):
+    _name = 'gk.doctor'
+    _description = 'Doctor'
+
+    name = fields.Char()
+
+    active = fields.Boolean(
+        default=True, )
+
